ad3a13105188fc8030ad44d15a40adf1dc1a1ed9
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-12 13:36:36 +0530

    Create project4continued_colour.py

---
+++ project4continued_colour.py
@@ -0,0 +1,11 @@
+
+print("Youll be asked a bunch of questions then we'll make you up an amazing story with YOU as the star")
+print()
+name=input("Your name: ")
+enemy=input("Your worst enemy's name: ")
+power=input("Your super power: ")
+print()
+
+print("Our story begins as our hero approaches a foreboding castle...")
+print("Suddenly a bolt of lightning struck the ground at the feet of", name)
+print("\033[31m","'Our final battle begins!' ","\033[0m", "shouts the evil", enemy," clearly missing the fact that ", name," has the power of", power, "which means they will never win quite easily") 
