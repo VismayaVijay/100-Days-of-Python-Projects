f71409ae7642f407d5d27541c1d76e19be58e37a
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-12 14:03:10 +0530

    Create Project5.py

---
+++ Project5.py
@@ -0,0 +1,9 @@
+name = input("what is your name?")
+print()
+if name == "Michael":
+  print('''Welcome Michael!!
+            It's amazing to see you!!''')
+
+else:
+  print("Hello " + name +"!")
+  print("It's wonderful to meet you for the very first time")
