b205462e8a1e93f29fb5b4acca468d50b072e542
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-12 14:21:45 +0530

    Create Project5__.py

---
+++ Project5__.py
@@ -0,0 +1,25 @@
+#Marvel Movie Character Creator
+
+print("Marvel Movie Character Creator")
+print()
+print()
+spider = input("Do you like hanging around? ")
+
+if spider == "Y":
+   print("Avengers Assemble")
+
+else:
+  print("Then you are not Spiderman")
+
+gravel =input ("Do you have a gravelly voice? ")
+if gravel == "Y":
+  print("Avengers Assemble")
+else:
+  print("Aww, then you are not Korg")
+
+marvel = input("Do you often feel Marvelous? ")
+
+if marvel == "Y":
+  print("Avengers Assemble")
+else:
+  print("Aww, then you are not Captain Marvel")
