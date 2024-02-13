e9745188c828484d7a3db190ec75a76e523b82e2
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-13 12:13:43 +0530

    Create Project6.py

---
+++ Project6.py
@@ -0,0 +1,19 @@
+print("MY LOGIN SYSTEM")
+print("---------------")
+print()
+
+username =input("Username: ")
+password =input ("Password: ")
+print()
+
+if username =="Bree" and password=="Bree":
+   print("E'llo there Bree!")
+
+elif username =="Grimes" and password =="grimes":
+   print("Nice to meetcha, Grimey boy")
+
+elif username =="Rick" and username == "Ricky":
+   print("Ricky, Micky went on a trip!")
+
+else:
+   print("Go away, stranger danger!!")
