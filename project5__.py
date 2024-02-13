0c06c7655870ffcb5985856c4e930456e55b352e
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-13 12:04:30 +0530

    Create project5__.py

---
+++ project5__.py
@@ -0,0 +1,12 @@
+print("Secure Login")
+username= input("Username: ")
+password= input("Password: ")
+print()
+if username == "David" and password== "password":
+  print("ello there, David!!")
+
+elif username == "Diana" and password == "suzanne12":
+  print("What a wonderful day, Diana!!")
+
+else:
+   print("Go away!!")
