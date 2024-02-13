a59dd073038e9d90476c79c35afd41b87724fde2
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-13 13:33:50 +0530

    Create Project2_Affirmation_Engine.py

---
+++ Project2_Affirmation_Engine.py
@@ -0,0 +1,44 @@
+print("Wholesome Positivity Machine")
+print("------------------------------")
+print()
+
+name=input("Who are you? \n")
+day=input("What's the day of the week?\n")
+
+if day == "Monday" or day=="monday":
+   print('''       Monday's here let's cheer and say
+                   Positive vibes will pave the way!
+                   So here's to Monday, let's make it grand,
+                   With optimism, we'll take a stand!''')
+elif day=="tuesday" or day=="Tuesday":
+   print('''           Tuesday's here, let's make it shine bright,
+                       With positivity, we'll take flight.
+                       So let's embrace this Tuesday cheer,
+                       With a smile that's oh so clear.''')
+elif day=="wednesday" or day=="Wednesday":
+ print('''           Wednesday's magic is ours to seize,
+                     Filled with laughter, love, and ease.
+                     In every moment, let's aim to please,
+                     For Wednesday brings opportunities to appease!''')
+elif day=="thursday" or day=="Thursday":
+ print('''           Seize the day with all your glee,
+                     Thursday's here, let's make it a spree!
+                     With laughter, love, and endless fun,
+                     We'll make this day second to none!"''')
+elif day=="friday" or day=="Friday":
+ print('''           Dance and sing, let spirits soar,
+                     On Friday's wings, we'll explore!
+                     Friday's magic, it's in the air,
+                     Let's make memories everywhere!''')
+elif day=="saturday" or day=="Saturday":
+ print('''           Seize the day with all your might,
+                     In your dreams, let your spirit take flight.
+                     This Saturday's yours, make it sublime,
+                     You're amazing, now go and shine!''')
+elif day=="sunday" or day=="Sunday":
+ print("Dear ",name,",",''' On Sundays bright, with skies so clear,
+                     Let joy and laughter draw near.
+                     In every moment,shine and glow,
+                     With gratitude, may your heart overflow.''')
+else:
+  print("You are out of days, matey!!")
