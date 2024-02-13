0c5737d008ca1661896f7b0d47b6ca672d02dd5b
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-13 11:56:04 +0530

    Create Project5_elif.py

---
+++ Project5_elif.py
@@ -0,0 +1,11 @@
+print("Secure Login")
+username= input("Username: ")
+print()
+if username == "David":
+  print("ello there, David!!")
+
+elif username == "Diana":
+  print("What a wonderful day, Diana!!")
+
+else:
+   print("Go away!!")
