b15eff6325b88bde64ee4a0f2ed0d92d72cab9f2
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-13 13:57:22 +0530

    Create Day9_Generation Identifier.py

---
+++ Day9_Generation Identifier.py
@@ -0,0 +1,21 @@
+#Int anf Float
+
+print("Generation Identifier")
+print("----------------------")
+print()
+
+year=int(input("Which year were you born?\n"))
+
+if year>=1925 and year<=1946:
+  print("You are a traditionalist to the core!!")
+
+elif year>=1947 and year<=1964:
+  print("Boomin baby boooomerrr!!")
+elif year>=1965 and year<=1981:
+  print("Generation X, Malcolm X")
+
+elif year>=1982 and year<=1995:
+  print("Millenials, so digital")
+
+elif year>=1996 and year<=2015:
+  print("Gen Z to the rescue")
