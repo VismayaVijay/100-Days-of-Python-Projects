7f59a9bc15dcfac6f34b1385e93ee65ca2abac64
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-13 12:56:23 +0530

    Create Project7.py

---
+++ Project7.py
@@ -0,0 +1,26 @@
+print("FAKE FAN FINDER")
+print("----------------")
+print()
+
+anime= input("What's your favourite anime? \n" )
+
+if anime == "One piece":
+  print("Bummer!")
+
+elif anime== "AOT":
+  
+  character =input("Oh really?! Name a character then.\n")
+  if character == "Mikasa":
+    print("You got that by pure chance.Okay then, what is her last name?")
+    last_name=input("")
+    if last_name =="Ackerman":
+       print("Hmph")
+    else:
+       print("FAKE FAN!!")
+  
+  else:
+    print("FAKE FAN!!")
+ 
+
+else:
+  print("Rest don't matter!!")
