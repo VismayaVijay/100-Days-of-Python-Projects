9338e71850c729bfc6751cd6b13845d0129cafbe
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-13 14:30:42 +0530

    Create Day10_calculation.py

---
+++ Day10_calculation.py
@@ -0,0 +1,18 @@
+print("Bill Divider")
+print("-------------")
+print()
+
+bill=float(input("What is the bill? \n"))
+people =int(input("What is the bill?\n"))
+answer=bill/people
+answer=round(answer,2)
+print("You all owe" ,answer)
+
+#--------------------------------------------------------------------------
+
+print(3.4*6.8)
+print(2467/4678)
+print(10**2)
+print(343//100)
+print(343/4)
+print(343%4)
