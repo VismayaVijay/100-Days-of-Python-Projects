8f21024ac495ac3abb9739c48a285186468f9d27
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-14 14:59:55 +0530

    Create day15_while.py

---
+++ day15_while.py
@@ -0,0 +1,27 @@
+#Fix an infinite loop by adding:
+#  variable +=1
+#This is just saying "Count to 10 by 1 each time." to make the loop end.
+#Don't forget, if your condition is a > then you might need to -=. This will subtract from the variable instead of adding to it.
+
+import emoji
+exit =""
+while exit!='yes':
+  print(emoji.emojize(":astonished_face:"))
+  exit=input("Exit?:")
+
+---------------------------------------------------------------------
+
+exit=""
+animal=""
+
+while exit != "yes":  
+   animal=input("What animal do you want?")
+   
+   if animal =="cow": 
+     print("A cow goes moo")
+   elif animal=="lemur":
+     print("Umm..the Lemur goes awooga")
+   else:
+    print("Idk what sound",animal,"makes.")
+
+   exit=input("Do you want to exit?")
