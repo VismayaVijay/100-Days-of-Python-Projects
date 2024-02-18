4b0cce84936041a8216f45ee129c4476c9d1ffdf
VismayaVijay <78965331+VismayaVijay@users.noreply.github.com>
2024-02-18 21:49:02 +0530

    Update day15_while.py

--- day15_while.py
+++ day15_while.py
@@ -25,3 +25,15 @@ while exit != "yes":
     print("Idk what sound",animal,"makes.")
 
    exit=input("Do you want to exit?")
+
+-----------------------------------------------------------------------------
+
+exit =''
+while exit!='yes':
+  print("yippee")
+  exit=input("Do you wanna exit? ")
+
+else:
+  print('I hate seeing you leave, but I love seeing you walk away!')
+  
+ 
